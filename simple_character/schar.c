c9f8d14997bb2cb2e36cfd4028c2ad8b362678e3
FernandezKA <fernandes.kir@yandex.ru>
2025-10-22 17:03:32 +0300

    [+] add simple char. driver without udev

---
+++ simple_character/schar.c
@@ -0,0 +1,105 @@
+/**
+ * @file schar.c
+ * @author FernandezKA (fernandes.kir@yandex.ru)
+ * @brief
+ * @version 0.1
+ * @date 2025-10-22
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+
+#define DEVICE_NAME "simple_char"
+#define BUF_SIZE 1024U
+
+static int major;
+static char device_buffer[BUF_SIZE];
+static int open_count = 0;
+
+static int schar_open(struct inode *inode, struct file *file)
+{
+    open_count++;
+    pr_info("%s: device_opened %d time(s) \n", DEVICE_NAME, open_count);
+    return 0;
+}
+
+static ssize_t schar_read(struct file *file, char __user *user_buf, size_t count, loff_t *offset)
+{
+    size_t available = BUF_SIZE - *offset;
+    size_t to_copy = min(count, available);
+
+    if (to_copy == 0)
+    {
+        return 0; // EOF
+    }
+
+    if (copy_to_user(user_buf, device_buffer + *offset, to_copy))
+    {
+        return -EFAULT;
+    }
+
+    *offset += to_copy;
+    pr_info("%s: read %zu bytes\n", DEVICE_NAME, to_copy);
+    return to_copy;
+}
+
+static ssize_t schar_write(struct file *file, const char __user *user_buf, size_t count, loff_t *offset)
+{
+    size_t available = BUF_SIZE - *offset;
+    size_t to_copy = min(count, available);
+
+    if (to_copy == 0)
+    {
+        return -ENOSPC;
+    }
+
+    if (copy_from_user(device_buffer + *offset, user_buf, to_copy))
+    {
+        return -EFAULT;
+    }
+
+    *offset += to_copy;
+    pr_info("%s: wrote %zu bytes\n", DEVICE_NAME, to_copy);
+    return to_copy;
+}
+
+static int schar_release(struct inode *inode, struct file *file) {
+    pr_info("%s: device closed\n", DEVICE_NAME);
+    return 0;
+}
+
+static const struct file_operations fops = {
+    .owner = THIS_MODULE,
+    .open = schar_open,
+    .read = schar_read,
+    .write = schar_write, 
+    .release = schar_release
+};
+
+static int __init schar_init(void) {
+    major = register_chrdev(0, DEVICE_NAME, &fops);
+    if (major < 0) {
+        pr_err("%s: failed to register char device\n", DEVICE_NAME);
+        return major;
+    }
+
+    pr_info("%s: registered with major number %d\n", DEVICE_NAME, major);
+    return 0;
+}
+
+static void __exit schar_exit(void) {
+    unregister_chrdev(major, DEVICE_NAME);
+    pr_info("%s: unregister device\n", DEVICE_NAME);
+}
+
+module_init(schar_init);
+module_exit(schar_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("FernandesKA");
+MODULE_DESCRIPTION("Simple Character Device Driver");
